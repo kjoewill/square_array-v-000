87087da232077fb3a002b004d9f2abc1de0a4daa
Kevin W <kjoewill@gmail.com>
2018-08-21 04:18:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   squared_array = []
+  sa_index = 0
   array.each do |element|
-    squared_array = each * each
+    squared_array[sa_index] = each * each
+    sa_index +=1
   end
   squared_array
 end
